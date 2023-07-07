b1698aac6a93ac6e5399a1549eb46403cc6ecb3f
Mateusz Jaroszko <matt7269@protonmail.com>
2023-07-07 20:44:42 +0200

    Dodaj pliki projektów.

---
+++ Models/StudentContext.cs
@@ -0,0 +1,27 @@
+﻿using ApiStudent.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiStudent.Models
+{
+    public class StudentContext : DbContext
+    {
+        
+
+        // Deklaracje zbiorów danych DbSet<T> i konfiguracje modelu
+        // ...
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Konfiguracje modelu
+            // ...
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public class StudentDbContext : IdentityDbContext<UserEntity,UserRole, int>
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
